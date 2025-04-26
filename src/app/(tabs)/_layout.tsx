60b145ae1f4bc1afa2559b23336fd06d9ce680ef
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2025-04-26 22:12:20 +0530

    Configure nativewindcss

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,74 +1,77 @@
 import { Tabs } from 'expo-router'
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? 'home-sharp' : 'home-outline'}
-              size={20}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="live"
-        options={{
-          title: 'Live',
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? 'bowling-ball' : 'bowling-ball-outline'}
-              size={20}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="series"
-        options={{
-          title: 'Series',
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? 'trophy-sharp' : 'trophy-outline'}
-              size={20}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="videos"
-        options={{
-          title: 'Videos',
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? 'videocam' : 'videocam-outline'}
-              size={20}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="news"
-        options={{
-          title: 'News',
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? 'newspaper' : 'newspaper-outline'}
-              size={20}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'home-sharp' : 'home-outline'}
+                size={20}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="live"
+          options={{
+            title: 'Live',
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'bowling-ball' : 'bowling-ball-outline'}
+                size={20}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="series"
+          options={{
+            title: 'Series',
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'trophy-sharp' : 'trophy-outline'}
+                size={20}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="videos"
+          options={{
+            title: 'Videos',
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'videocam' : 'videocam-outline'}
+                size={20}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="news"
+          options={{
+            title: 'News',
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'newspaper' : 'newspaper-outline'}
+                size={20}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context'
+
+import '../../global.css'
 
 export default function RootLayout() {
   return (
